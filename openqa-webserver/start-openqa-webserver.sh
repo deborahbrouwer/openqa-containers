96d8076a7d7f6dd22227a7047cfda4b0931517c8
fedora Cloud User <fedora@webstg.qa>
2024-04-08 18:26:27 +0000

    websrv service use start script

---
+++ openqa-webserver/start-openqa-webserver.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# If running locally, add values e.g.
+# SRV='/home/fedora/openqa-containers/openqa-webserver' DETACHED=yes LOCAL_PORTS=yes ./start-openqa-webserver.sh
+
+set -e
+
+if [ ! -d "${SRV}/hdd" ] && [ ! -L "${SRV}}/hdd" ]; then
+	mkdir "${SRV}/hdd"
+fi
+
+if [ ! -d "${SRV}/iso" ] && [ ! -L "${SRV}}/iso" ]; then
+	mkdir "${SRV}/iso"
+fi
+
+if [ ! -d "${SRV}/data" ] && [ ! -L "${SRV}}/data" ]; then
+	echo "Missing ${SRV}/data"
+    exit
+fi
+
+if [ ! -f "${SRV}/conf/client.conf" ]; then
+	echo "Missing ${SRV}/conf/client.conf"
+    exit
+fi
+
+if [[ "$DETACHED" == "true" ]] || [[ "$DETACHED" == "yes" ]]; then
+    detached_arg="-d"
+fi
+
+ports_arg="-p 80:80 -p 443:443"
+if [[ "$LOCAL_PORTS" == "true" ]] || [[ "$LOCAL_PORTS" == "yes" ]]; then
+    ports_arg="-p 8080:80 -p 1443:443"
+fi
+
+/usr/bin/podman run --rm -i --name openqa-webserver \
+	${ports_arg} \
+	${detached_arg} \
+	--network=slirp4netns \
+	-v ${SRV}/hdd:/var/lib/openqa/share/factory/hdd:z \
+	-v ${SRV}/iso:/var/lib/openqa/share/factory/iso:z \
+	-v ${SRV}/data:/var/lib/pgsql/data/:z \
+	-v ${SRV}/conf:/conf/:z \
+	-v ${SRV}/init_openqa_web.sh:/init_openqa_web.sh:z \
+	localhost/openqa-webserver:latest /init_openqa_web.sh
+

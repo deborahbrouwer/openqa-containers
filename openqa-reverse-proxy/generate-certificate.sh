933a404d966fa2f9a777002e3790873cdc5ab30c
fedora Cloud User <fedora@websrv.qa>
2024-04-12 04:41:48 +0000

    reverse-proxy: cert generation fixes

--- openqa-reverse-proxy/generate-certificate.sh
+++ openqa-reverse-proxy/generate-certificate.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
+
+# Find the new certs in /var/lib/httpd/md/domains/openqa.fedorainfracloud.org/
+# Copy them into /conf so that they will persist beyond the container's lifetime
+
 set -e
 
-    echo "Generating new certificates"
-    sed -i '/#ServerName www.example.com:80/a\ServerName openqa.fedorainfracloud.org' /etc/httpd/conf/httpd.conf
+echo "Generating new certificates"
+sed -i '/#ServerName www.example.com:80/a\ServerName openqa.fedorainfracloud.org' /etc/httpd/conf/httpd.conf
 
-    # Use the configs that include mod_md
-    ln -s /conf/openqa-ssl.conf /etc/httpd/conf.d/openqa-ssl.conf
-    ln -s /conf/openqa.conf /etc/httpd/conf.d/openqa.conf
+# Enable the mod_md module
+sed -i '/<IfModule mod_md.c>/,/<\/IfModule>/ s/^#\?//' /conf/openqa-proxy-ssl.conf
 
-    # Apache mod_md will request them the configs from the MDCertificateAuthority as
-    # specified in openqa-ssl.conf but only if there are no other certificates available,
+# Apache mod_md will request them the configs from the MDCertificateAuthority as
+# specified in openqa-ssl.conf but only if there are no other certificates configured
+sed -i '/SSLCertificateFile/s/^/#/' /etc/httpd/conf.d/ssl.conf
+sed -i '/SSLCertificateKeyFile/s/^/#/' /etc/httpd/conf.d/ssl.conf
 
-    # remove localhost cert
-    rm -rf /etc/pki/tls/certs/localhost.crt
-    rm -rf /etc/pki/tls/private/localhost.key
-    sed -i '/SSLCertificateFile/s/^/#/' /etc/httpd/conf.d/ssl.conf
-    sed -i '/SSLCertificateKeyFile/s/^/#/' /etc/httpd/conf.d/ssl.conf
+# don't look for local or pubcert
+sed -i '/SSLCertificateFile/s/^/#/' /conf/openqa-proxy-ssl.conf
+sed -i '/SSLCertificateKeyFile/s/^/#/' /conf/openqa-proxy-ssl.conf
 
-    # don't look for pubcert
-    sed -i '/SSLCertificateFile/s/^/#/' /conf/openqa-ssl.conf
-    sed -i '/SSLCertificateKeyFile/s/^/#/' /conf/openqa-ssl.conf
+# restart
+httpd -DSSL
 
-    # New certs will be in /var/lib/httpd/md/domains/openqa.fedorainfracloud.org/
-    # Copy them outside of the container to bind them in again later
-    # see https://github.com/icing/mod_md
-  fi
-
-}
+cp /var/lib/httpd/md/domains/openqa.fedorainfracloud.org/pubcert.pem /conf/pubcert.pem || true
+cp /var/lib/httpd/md/domains/openqa.fedorainfracloud.org/privkey.pem /conf/privkey.pem || true
--- openqa-reverse-proxy/init-openqa-reverse-proxy.sh
+++ openqa-reverse-proxy/init-openqa-reverse-proxy.sh
@@ -1,50 +1,51 @@
 #!/bin/bash
 set -ex
 function cleanup() {
-  echo "Return ownership of bound directories back to container host."
-  chown -R root:root /etc/httpd/logs
-  exit
+echo "Return ownership of bound directories back to container host."
+chown -R root:root /etc/httpd/logs
+exit
 }
 trap cleanup SIGTERM SIGINT
 
-function configure_apache() {
 
-  # Production configuration
-  if [ -f "/conf/privkey.pem" ]; then
-    echo "Using production SSL/TLS certificates"
+CONFIG_DIR=/etc/httpd/conf.d/
 
-    ln -s /conf/pubcert.pem /etc/pki/tls/certs/pubcert.pem
-    ln -s /conf/privkey.pem /etc/pki/tls/private/privkey.pem
-
-    sed -i '/#ServerName www.example.com:80/a\ServerName openqa.fedorainfracloud.org' /etc/httpd/conf/httpd.conf
-
-    # Use the configs that include mod_md
-    ln -s /conf/openqa-ssl.conf /etc/httpd/conf.d/openqa-ssl.conf
-    ln -s /conf/openqa.conf /etc/httpd/conf.d/openqa.conf
-  else
-    # Local configuration relies on on /etc/httpd/conf.d/ssl.conf 
-    echo "Using local SSL/TLS certificates"
-
-    local mojo_resources=$(perl -e 'use Mojolicious; print(Mojolicious->new->home->child("Mojo/IOLoop/resources"))')
-    cp "$mojo_resources"/server.crt /etc/httpd/tls/localhost.crt
-    cp "$mojo_resources"/server.key /etc/httpd/tls/localhost.key
-    cp "$mojo_resources"/server.crt /etc/httpd/tls/ca.crt
-  fi
-}
-
-# RUN dnf install -y openqa-httpd mod_ssl mod_proxy_html mod_session mod_md
 dnf install -y mod_md perl perl-Mojolicious procps-ng mod_proxy_html
 
-
-ln -s /conf/openqa-proxy-ssl.conf /etc/httpd/conf.d/openqa-proxy-ssl.conf
-ln -s /conf/openqa-proxy.conf /etc/httpd/conf.d/openqa-proxy.conf
-ln -s /conf/openqa-proxy-common.inc /etc/httpd/conf.d/openqa-proxy-common.inc
+ln -s /conf/openqa-proxy-ssl.conf `$CONFIG_DIR/openqa-proxy-ssl.conf`
+ln -s /conf/openqa-proxy.conf `$CONFIG_DIR/openqa-proxy.conf`
 
 ln -s /etc/httpd/conf.modules.d/00-base.conf /etc/httpd/conf.modules.d/00-base.load
 ln -s /etc/httpd/conf.modules.d/00-ssl.conf /etc/httpd/conf.modules.d/00-ssl.load
 /usr/libexec/httpd-ssl-gencerts
 
-configure_apache
+
+# Production configuration
+if [ -f "/conf/privkey.pem" ]; then
+  echo "Using production SSL/TLS certificates"
+
+  ln -s /conf/pubcert.pem `$CONFIG_DIR/pubcert.pem`
+  ln -s /conf/privkey.pem `$CONFIG_DIR/privkey.pem`
+
+  sed -i "s/SSLCertificateFile .*/SSLCertificateFile `$CONFIG_DIR/pubcert.pem`/" `$CONFIG_DIR/openqa-proxy-ssl.conf`
+  sed -i "s/SSLCertificateKeyFile .*/SSLCertificateKeyFile `$CONFIG_DIR/privkey.pem`/" `$CONFIG_DIR/openqa-proxy-ssl.conf`
+
+  sed -i '/#ServerName www.example.com:80/a\ServerName openqa.fedorainfracloud.org' /etc/httpd/conf/httpd.conf
+
+else
+  echo "Using local SSL/TLS certificates"
+
+  local mojo_resources=$(perl -e 'use Mojolicious; print(Mojolicious->new->home->child("Mojo/IOLoop/resources"))')
+  cp "$mojo_resources"/server.crt `$CONFIG_DIR/localhost.crt`
+  cp "$mojo_resources"/server.key `$CONFIG_DIR/localhost.key`
+  cp "$mojo_resources"/server.crt `$CONFIG_DIR/ca.crt`
+
+  sed -i "s/SSLCertificateFile .*/SSLCertificateFile `$CONFIG_DIR/localhost.crt`/" `$CONFIG_DIR/openqa-proxy-ssl.conf`
+  sed -i "s/SSLCertificateKeyFile .*/SSLCertificateKeyFile `$CONFIG_DIR/localhost.key`/" `$CONFIG_DIR/openqa-proxy-ssl.conf`
+
+  sed -i 's/ServerName .*/ServerName www.example.com:80' /etc/httpd/conf/httpd.conf
+
+fi
 
 httpd -DSSL || true
 

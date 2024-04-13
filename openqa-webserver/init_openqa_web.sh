39ab05c06cd3d1fb00c4f68a4a9fac3905a1bebe
fedora Cloud User <fedora@websrv.qa>
2024-04-13 01:32:15 +0000

    websrv: remove cert gen

--- openqa-webserver/init_openqa_web.sh
+++ openqa-webserver/init_openqa_web.sh
@@ -50,34 +50,6 @@ function configure_apache() {
     cp "$mojo_resources"/server.crt /etc/pki/tls/certs/ca.crt
   fi
 
-  # For convenience, this is the option to generate certs for production configuration,
-  # but it wouldn't usually be needed
-  if [[ "$GENERATE_CERTS" == "true" ]] || [[ "$GENERATE_CERTS" == "yes" ]]; then
-    echo "Generating new certificates"
-    sed -i '/#ServerName www.example.com:80/a\ServerName openqa.fedorainfracloud.org' /etc/httpd/conf/httpd.conf
-
-    # Use the configs that include mod_md
-    ln -s /conf/openqa-ssl.conf /etc/httpd/conf.d/openqa-ssl.conf
-    ln -s /conf/openqa.conf /etc/httpd/conf.d/openqa.conf
-
-    # Apache mod_md will request them the configs from the MDCertificateAuthority as
-    # specified in openqa-ssl.conf but only if there are no other certificates available,
-
-    # remove localhost cert
-    rm -rf /etc/pki/tls/certs/localhost.crt
-    rm -rf /etc/pki/tls/private/localhost.key
-    sed -i '/SSLCertificateFile/s/^/#/' /etc/httpd/conf.d/ssl.conf
-    sed -i '/SSLCertificateKeyFile/s/^/#/' /etc/httpd/conf.d/ssl.conf
-
-    # don't look for pubcert
-    sed -i '/SSLCertificateFile/s/^/#/' /conf/openqa-ssl.conf
-    sed -i '/SSLCertificateKeyFile/s/^/#/' /conf/openqa-ssl.conf
-
-    # New certs will be in /var/lib/httpd/md/domains/openqa.fedorainfracloud.org/
-    # Copy them outside of the container to bind them in again later
-    # see https://github.com/icing/mod_md
-  fi
-
 }
 
 function upgradedb() {
--- openqa-webserver/start-openqa-webserver.sh
+++ openqa-webserver/start-openqa-webserver.sh
@@ -38,7 +38,6 @@ podman run --rm -i --name openqa-webserver \
 	-p 8080:80 -p 1443:443 \
 	${detached_arg} \
 	--network=slirp4netns \
-	-e GENERATE_CERTS=$GENERATE_CERTS \
 	-v ${SRV}/hdd:/var/lib/openqa/share/factory/hdd:z \
 	-v ${SRV}/iso:/var/lib/openqa/share/factory/iso:z \
 	-v ${SRV}/data:/var/lib/pgsql/data/:z \
